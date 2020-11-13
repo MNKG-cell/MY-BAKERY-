570a4bf6c5cb5eb71b41fa82eb4512c6b3698fd1
MAUREEN <maureennayebare2@gmail.com>
2020-11-13 22:45:12 +0300

    Bakery detail

---
+++ main.js
@@ -0,0 +1,11 @@
+function myFunc(){document.getElementById('btn')};
+
+btn.onclick=function myFunc(){alert('To place an order, please fill in your details in the form below and then press the submit button. Thank you');};
+
+console.log(alerted);
+
+function myFunction(){document.getElementById=('sub')};
+sub.onclick= function myFunction(){alert('Thank you for trusting us with your information. We shall contact your email to let you know of when we will deliver your items.' );};
+
+myFunction();
+console.log(alerted);
